c9b5b189534185ac7c6455601c6ff7a031d712d1
amisu <hamisurahman@gmail.com>
2024-01-13 05:24:43 +0000

    User signup

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 import dotenv from "dotenv"
+import connectDB from './db/connectDB.js'
+import cookieParser from 'cookie-parser'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
+
+connectDB();
 const app = express()
 
 const PORT = process.env.PORT || 3000 ;
 
-app.listen(5000, () => console.log(`Server started at http://localhost:${PORT}`))
+app.use(express.json()); // To parse JSON data in the req.body
+app.use(express.urlencoded({extended:true})) //To parse form data in the req.body
+
+app.use('/api/users', userRoutes)
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
